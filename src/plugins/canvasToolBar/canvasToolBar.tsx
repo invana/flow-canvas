a4f5e0f4cd4505cdff899ab46f7fc8df3de29a01
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-12-15 20:34:24 +0530

    canvas toolbar basic implementation

--- src/plugins/canvasToolBar/canvasToolBar.tsx
+++ src/plugins/canvasToolBar/canvasToolBar.tsx
@@ -8,9 +8,11 @@ import {
   useReactFlow,
   PanelProps,
 } from "@xyflow/react";
+import { Separator } from "@/components/ui/separator"
 import { Slider } from "@/components/ui/slider";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import ButtonWithTooltip from "@/components/extended/button-with-tooltip";
 
 
 const CanvasToolBar = React.forwardRef<
@@ -20,58 +22,63 @@ const CanvasToolBar = React.forwardRef<
   const { zoom } = useViewport();
   const { zoomTo, zoomIn, zoomOut, fitView } = useReactFlow();
 
-  const { minZoom, maxZoom } = useStore(
-    (state) => ({
-      minZoom: state.minZoom,
-      maxZoom: state.maxZoom,
-    }),
-    (a, b) => a.minZoom !== b.minZoom || a.maxZoom !== b.maxZoom,
-  );
+  // const { minZoom, maxZoom } = useStore(
+  //   (state) => ({
+  //     minZoom: state.minZoom,
+  //     maxZoom: state.maxZoom,
+  //   }),
+  //   (a, b) => a.minZoom !== b.minZoom || a.maxZoom !== b.maxZoom,
+  // );
+
+
 
   return (
     <Panel
       className={cn(
-        "flex gap-1 rounded-md bg-primary-foreground p-1 text-foreground",
-        className,
+      "flex gap-1 rounded-md bg-primary-foreground text-foreground text-[12px] border",
+      className,
       )}
       {...props}
     >
-      <Button
+      <ButtonWithTooltip
         variant="ghost"
         size="icon"
         onClick={() => zoomOut({ duration: 300 })}
+        tooltip={<p>Zoom out</p>}
       >
         <Minus className="h-4 w-4" />
-      </Button>
-      <Slider
+      </ButtonWithTooltip>
+      {/* <Slider
         className="w-[140px]"
         value={[zoom]}
         min={minZoom}
         max={maxZoom}
         step={0.01}
         onValueChange={(values) => zoomTo(values[0])}
+      /> */}
+      <input
+        className="w-10 tabular-nums"
+        onChange={(e) => zoomTo(Number(e.target.value) / 100, { duration: 300 })}
+        value={(100 * zoom).toFixed(0)}
       />
-      <Button
+      <ButtonWithTooltip
         variant="ghost"
         size="icon"
         onClick={() => zoomIn({ duration: 300 })}
+        tooltip={<p>Zoom In</p>}
       >
         <Plus className="h-4 w-4" />
-      </Button>
-      <Button
-        className="min-w-20 tabular-nums"
-        variant="ghost"
-        onClick={() => zoomTo(1, { duration: 300 })}
-      >
-        {(100 * zoom).toFixed(0)}%
-      </Button>
-      <Button
+      </ButtonWithTooltip>
+      <ButtonWithTooltip
         variant="ghost"
         size="icon"
         onClick={() => fitView({ duration: 300 })}
+        tooltip={<p>Fitview</p>}
       >
         <Maximize className="h-4 w-4" />
-      </Button>
+      </ButtonWithTooltip>
+      <Separator orientation="vertical" />
+
     </Panel>
   );
 });
